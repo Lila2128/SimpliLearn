8eca008fb63dad5134eb414f5e4585043a32cb1e
LILABATI MOHANTA <114357258+Lila2128@users.noreply.github.com>
2022-09-29 01:07:43 +0530

    Add files via upload
    Virtual Key for Your Repositories

--- Driver.java
+++ Driver.java
@@ -5,19 +5,22 @@ import java.util.Scanner;
 public class Driver {
 	
 	public static void main(String [] args) {
-//		final String ROOT_DIRECTORY_PATH ="E://Java_Practice//Test_Files";
-		String directoryName;
+
+		String directoryName = null;
 		String FileName;
 		
 		LockedMeBo bo = new LockedMeBo();
 		
+//		fetching data from user.
 		Scanner sc = new Scanner(System.in);
 		
-		
+//		displaying --------------WELCOME SCREEN------------------------
 		System.out.println("Welcome to LockedMe.com");
 		System.out.println("Check out our Products listings.");
 		System.out.println("\n");
 		int ch, ch1;
+		
+//--------------------------------start of user interaction information-----------------------------------------
 		do
 		{
 			System.out.println("1. Fetch List of files");
@@ -25,18 +28,23 @@ public class Driver {
 			System.out.println("3. Exit from the application");
 			System.out.println("Please Enter your choice");
 			ch = sc.nextInt();
-			directoryName = readDirectoryName(sc);
+			if (ch < 3)
+				directoryName = readDirectoryName(sc);
+			
 			switch(ch) {
 			case 1: 
-				
+//	---------------------------------to arrange files in ascending order.----------------------
 				bo.displayFilesInAscendingOrder(directoryName);
+				System.out.println("\n");
 				break;
 			case 2:
+//	-----------------------------------Start of business level operations---------------------------------------
 				do
 				{
 					System.out.println("1. Add File to the Directory");
 					System.out.println("2. Remove file from Directory");
 					System.out.println("3. Searching a file in Directory");
+					System.out.println("4. Return to the main context");
 					System.out.println(" Enter your choice");
 					ch1 = sc.nextInt();
 					switch(ch1)
@@ -44,21 +52,27 @@ public class Driver {
 					case 1:
 						FileName =readFileName(sc);
 						bo.addFile(FileName, directoryName);
+						System.out.println("\n");
 						break;
 					case 2:
 						FileName = readFileName(sc);
 						bo.deleteFile(FileName, directoryName);
+						System.out.println("\n");
 						break;
 					case 3:
 						FileName = readFileName(sc);
 						bo.searchFile(FileName, directoryName);
+						System.out.println("\n");
 						break;
 					}
 				}while(ch1<=3);
 			}
-		}while(ch < 3);
+		}while(ch <= 2);
 	}
 
+//	end of the Operations.
+	
+//	Fetching File Name from the user.
 	private static String readFileName(Scanner sc) {
 		System.out.println("enter the file name to edit");
 		sc.nextLine();
@@ -66,6 +80,7 @@ public class Driver {
 		return filename;
 	}
 	
+//	Fetching Directory Name from the user.
 	private static String readDirectoryName(Scanner sc) {
 		System.out.println("Enter the Directory name you want to edit from.");
 		sc.nextLine();
--- LockedMeBo.java
+++ LockedMeBo.java
@@ -3,11 +3,14 @@ package com.lockedme;
 import java.io.File;
 
 public class LockedMeBo {
+//	Start of displaying Files in ascending order and sorting them using bubble sort.
+	
 	public void displayFilesInAscendingOrder(String directoryPath)
 	{
 		try {
 			File f = new File(directoryPath);
 			File files[] =f.listFiles();
+			sort(files, files.length);
 			for(File fileEntry : files)
 			{
 				if(fileEntry.isFile())
@@ -18,14 +21,35 @@ public class LockedMeBo {
 		}	
 	}
 	
-
+	private void sort(File[] files, int n) {
+		int x, j, swaps;
+		File temp;
+		for(x=n-2; x>=0; x--) {
+			swaps =0;
+			for(j=0; j<= x; j++) {
+				if(files[j].getName().compareTo(files[j+1].getName())>0)
+					{
+					temp = files[j];
+					files[j]= files[j+1];
+					files[j+1]=temp;
+					swaps++;
+					}
+			}
+			if(swaps==0)
+				break;
+		}
+		
+	} 
+//End of SORTING and ARRANGING files.
+	
+//	Start of ADDING a file to the directory path.
 	public void addFile(String fileName, String directoryPath)
 	{
 		try {
 			File directory = new File(directoryPath);
 			if(directory.isDirectory()) 
 			{	
-				File f = new File(directoryPath + "/" + fileName); //to fetch the desired directory.
+				File f = new File(directoryPath + "/" + fileName); //to fetch the desired directory and file from user.
 				if(!f.exists()) {
 					f.createNewFile();
 					System.out.println("File successfully created");
@@ -39,13 +63,16 @@ public class LockedMeBo {
 		}
 	}
 	
+//End of ADDING a file.
+	
+//	Start of DELETING file.
 	public void deleteFile(String fileName, String directoryPath)
 	{
 		boolean isDeleted = false;
 		boolean isExisted = false;
 		try {
-			File f = new File(directoryPath);
-			File files[] = f.listFiles();
+			File f = new File(directoryPath); //fetching the directory of the files.
+			File files[] = f.listFiles(); //assigning files to an array to search for the desired file.
 			for (File fileEntry : files) {
 				if(fileEntry.getName().equals(fileName)) {
 					isExisted= true;
@@ -61,6 +88,9 @@ public class LockedMeBo {
 			e.printStackTrace();
 		}
 	}
+//	End of DELETING files.
+
+//	start of SEARCH file.
 	
 	public void searchFile(String fileName, String directoryPath)
 	{ 
@@ -84,4 +114,6 @@ public class LockedMeBo {
 			e.printStackTrace();
 		}
 	}
+//	End of SEARCH file.
+	
 }
